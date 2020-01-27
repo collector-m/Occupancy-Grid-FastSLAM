845a2a37c3a1124d20d3c187545feb5b3fd66fad
matssteinweg <mats.steinweg@me.com>
2020-01-27 08:55:49 +0100

    Update Area Class
    restructure and add comments

--- Area.cpp
+++ Area.cpp
@@ -10,14 +10,18 @@
 #include <stdio.h>
 #include "Area.h"
 
-#define PI 3.14159265
-
 class Map;
 
-// standard constructor
+#define PI 3.14159265
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++ Constructor ++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Standard constructor
 Area::Area(){
     
-    // set member variables with default values
     this->x_min = -5.0;
     this->x_max = 25.0;
     this->y_min = -5.0;
@@ -26,126 +30,122 @@ Area::Area(){
     this->data_dim_x = (int) ((x_max - x_min) / resolution);
     this->data_dim_y = (int) ((y_max - y_min) / resolution);
     this->data = cv::Mat(this->data_dim_x, this->data_dim_y, CV_8UC3, cv::Scalar::all(255));
+    this->data.copyTo(scene_data);
 
 }
 
 
-// constructor
+// Constructor
 Area::Area(vector<vector<float>> wall_coordinates, float x_min, float x_max, float y_min, float y_max, float resolution){
     
-    // set wall coordinates and boundaries of the area
+    // Set wall coordinates and boundaries of the area
     this->wall_coordinates = wall_coordinates;
     this->x_min = x_min;
     this->x_max = x_max;
     this->y_min = y_min;
     this->y_max = y_max;
     
-    // verify that all walls are located inside the specified area
+    // Verify that all walls are located inside the specified area
     for (vector<vector<float>>::iterator it = wall_coordinates.begin(); it != wall_coordinates.end(); it++){
         const float x_min_wall = min((*it)[0], (*it)[2]);
         const float x_max_wall = max((*it)[0], (*it)[2]);
         const float y_min_wall = min((*it)[1], (*it)[3]);
         const float y_max_wall = max((*it)[1], (*it)[3]);
         if(x_min_wall < x_min || x_max_wall > x_max || y_min_wall < y_min || y_max_wall > y_max) {
-            cout << " At least one wall is located outside the specified area!" << endl;
+            cout << " At least one wall is located outside of the specified area!" << endl;
             exit(1);
         }
     }
     
-    // specify resolution and create image for drawing the area
+    // Specify resolution and create image for drawing area
     this->resolution = resolution;
     this->data_dim_x = (int) ((x_max - x_min) / resolution);
     this->data_dim_y = (int) ((y_max - y_min) / resolution);
     this->data = cv::Mat(this->data_dim_x, this->data_dim_y, CV_8UC3, cv::Scalar::all(255));
+    this->data.copyTo(scene_data);
     
-    // draw layout of the area
+    // Draw layout of the area
     this->drawWalls();
     
 }
 
 
-// *****************
-// drawing functions
-// *****************
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++ Print Summary +++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-void Area::drawInitialScene(){
+void Area::summary(){
     
-        // create a copy to restore original image
-        cv::Mat data_copy;
-        this->data.copyTo(data_copy);
-        
-        // draw robot
-        this->drawRobot();
-        
-        cv::imshow("Area", this->data);
-        cv::waitKey();
-        
-        // restore original image
-        this->data = data_copy;
-    
+    cout << "Area: " << endl;
+    cout << "-----" << endl;
+    cout << "Width: " << this->x_max - this->x_min << "m" << endl;
+    cout << "Height: " << this->y_max - this->y_min << "m" << endl;
+    // Print map summary
+    Map::summary();
+    // Print robot summary
+    this->getRobot().summary();
+
 }
 
 
-cv::Mat Area::drawScene(int verbose){
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++ Drawing Functions ++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Draw entire scene content according to specified verbosity level
+void Area::drawScene(int verbose){
         
-        // draw robot path
+        // ++++++++++++++++++++++++++++++++++++ Draw Static Content ++++++++++++++++++++++++++++++++++++++++++
+
+        // Draw robot path. Path is drawn before copying data to display all past locations of robot.
         this->drawPath();
             
-        // create a copy to restore original image
+    
+        // +++++++++++++++++++++++++++++++++++++++ Create Copy +++++++++++++++++++++++++++++++++++++++++++++++
+
+        // Create a copy to restore static scene at every time step
         cv::Mat data_copy;
         this->data.copyTo(data_copy);
-            
-        // draw sensor readings
+    
+    
+        // +++++++++++++++++++++++++++++++++++ Draw Dynamic Content ++++++++++++++++++++++++++++++++++++++++++
+
+        // Draw sensor readings
         this->drawScan();
         
-        // draw scan estimate
+        // Draw scan estimate if specified
         if (verbose == 2){
-        this->drawScanEstimate();
+            this->drawScanEstimate();
         }
             
-        // draw robot
+        // Draw robot on top of scans
         this->drawRobot();
         
-        // draw particles
+        // Draw particles if specified
         if (verbose >= 1){
-        this->drawParticles();
+            this->drawParticles();
         }
     
-        // create copy of scene for saving
-        cv::Mat data_save;
-        this->data.copyTo(data_save);
+        // Assign complete scene data for current time step to variable for result saving
+        this->data.copyTo(this->scene_data);
 
+        // Display current scene
         cv::imshow("Area", this->data);
         cv::waitKey(1);
     
-        // restore original image
+        // Restore static scene data
         this->data = data_copy;
     
-        return data_save;
-
 }
 
 
-void Area::drawPath(){
-    
-    // get robot pose in continuous world coordinates
-    Eigen::Vector2f robot_location = this->robot.getPose().block<2, 1>(0, 0);
-    
-    // get robot pose in discrete area coordinates
-    Eigen::Vector2i area_location = this->discretize_world_location(robot_location);
-    
-    // draw robot as a black circle
-    cv::circle(this->data, {area_location(0), area_location(1)}, 4, this->path_ground_truth_color, -1);
-    
-}
-
-
+// Draw walls
 void Area::drawWalls(){
     
-    // iterate over each wall
+    // Iterate over all walls in area
     for (vector<vector<float>>::iterator i = this->wall_coordinates.begin(); i != this->wall_coordinates.end(); i++) {
         
-        // extract start and end point of the line segment and transform them to discrete area coordinates
+        // Extract start and end point of the line segment and transform them to discrete area coordinates
         Eigen::Vector2f startpoint;
         Eigen::Vector2f endpoint;
         startpoint << (*i)[0], (*i)[1];
@@ -153,155 +153,169 @@ void Area::drawWalls(){
         Eigen:: Vector2i area_startpoint = this->discretize_world_location(startpoint);
         Eigen:: Vector2i area_endpoint = this->discretize_world_location(endpoint);
         
-        // draw a black line
+        // Draw wall as black line
         line(this->data, {area_startpoint(0), area_startpoint(1)}, {area_endpoint(0), area_endpoint(1)}, {0, 0, 0}, 2);
         
     }
+}
+
+
+// Draw robot path
+void Area::drawPath(){
     
-    cv::namedWindow("Area", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Area", this->data);
+    // Get robot pose in continuous world coordinates
+    Eigen::Vector2f robot_location = this->robot.getPose().block<2, 1>(0, 0);
+    
+    // Get robot pose in discrete area coordinates
+    Eigen::Vector2i area_location = this->discretize_world_location(robot_location);
+    
+    // Draw path as dots
+    cv::circle(this->data, {area_location(0), area_location(1)}, 4, this->path_ground_truth_color, -1);
     
 }
 
 
+// Draw laser scan
 void Area::drawScan(){
     
-    // get robot location in world coordinates
+    // Get robot location in world coordinates
     const Eigen::Vector2f robot_location = this->robot.getPose().block<2,1>(0,0);
     
-    // get robot heading
+    // Get robot heading
     const float robot_heading = this->robot.getPose()(2);
     
-    // get robot location in discrete area coordinates
+    // Get robot location in discrete area coordinates
     const Eigen::Vector2i area_robot_location = this->discretize_world_location(robot_location);
     
-    // get reference to measurements
+    // Get reference to measurements
     const Eigen::MatrixX2f measurement_ref = this->robot.getSensor().getMeasurements();
     
-    // get number of measurements
+    // Get number of measurements
     const int n_beams = this->robot.getSensor().getN();
     
-    // iterate over all measurements
+    // Iterate over all measurements
     for (int beam_id = 0; beam_id < n_beams; beam_id++) {
         
-        // get range and angle information for currently inspected measurement
+        // Get range and angle information for currently inspected measurement
         float phi = measurement_ref(beam_id, 0);
         float area_range = this->scale_world_variable(measurement_ref(beam_id, 1));
         
-        // get final angle as sum of robot's heading and measurement angle relative to robot
+        // Get final angle as sum of robot's heading and measurement angle relative to robot
         float alpha = phi + robot_heading;
-        alpha = fmod((alpha+PI), (2.0*PI)) - PI; // keep angle within range of (-PI/2, PI/2]
+        alpha = fmod((alpha+PI), (2.0*PI)) - PI; // Keep angle within range of [-pi, pi)
         
-        // get endpoint of the obtained measurement
+        // get Endpoint of the obtained measurement
         int line_endpoint_x = (int)(area_robot_location(0) + area_range * cos(alpha));
         int line_endpoint_y = (int)(area_robot_location(1) + area_range * sin(alpha));
         
-        // draw line
+        // Draw laser beam as line
         cv::line(this->data, {area_robot_location(0), area_robot_location(1)}, {line_endpoint_x, line_endpoint_y}, this->scan_color, 1);
     }
-    
 }
 
 
+// Draw laser scan estimate for each particle
 void Area::drawScanEstimate(){
     
     for (list<Particle>::iterator it = this->robot.getFilter().getParticles().begin(); it != this->robot.getFilter().getParticles().end(); it++) {
         
-        // get particle location in continuous world coordinates
+        // Get particle location in continuous world coordinates
         const Eigen::Vector2f particle_location = (*it).getPose().block<2,1>(0,0);
         
-        // get particle heading
+        // Get particle heading
         const float particle_heading = (*it).getPose()(2);
         
-        // get particle location in discrete area coordinates
+        // Get particle location in discrete area coordinates
         const Eigen::Vector2i area_particle_location = this->discretize_world_location(particle_location);
         
-        // get number of measurements
+        // Get number of measurements
         const int n_beams = this->robot.getSensor().getN();
         
-        // get reference to measurements
+        // Get reference to estimated measurements
         const Eigen::MatrixX2f measurement_estimate_ref = (*it).getMeasurementEstimate();
         
-        // iterate over all measurements
+        // Iterate over all estimated measurements
         for (int beam_id = 0; beam_id < n_beams; beam_id++) {
             
-            // get range and angle information for currently inspected measurement
+            // Get range and angle information for currently inspected measurement estimate
             float phi = measurement_estimate_ref(beam_id, 0);
             float area_range = this->scale_world_variable(measurement_estimate_ref(beam_id, 1));
             
-            // get final angle as sum of robot's heading and measurement angle relative to robot
+            // Get final angle as sum of robot's heading and measurement angle relative to robot
             float alpha = phi + particle_heading;
-            alpha = fmod((alpha+PI), (2.0*PI)) - PI; // keep angle within range of (-PI/2, PI/2]
+            alpha = fmod((alpha+PI), (2.0*PI)) - PI; // Keep angle within range of [-pi, pi)
             
-            // get endpoint of the obtained measurement
+            // Get endpoint of the obtained measurement
             int line_endpoint_x = (int)(area_particle_location(0) + area_range * cos(alpha));
             int line_endpoint_y = (int)(area_particle_location(1) + area_range * sin(alpha));
             
-            // draw line
+            // Draw laser beam estimate as line
             cv::line(this->data, {area_particle_location(0), area_particle_location(1)}, {line_endpoint_x, line_endpoint_y}, this->scan_estimate_color, 1);
         }
     }
 }
 
 
+// Draw robot
 void Area::drawRobot(){
     
-    // get robot location in world coordinates
+    // Get robot location in world coordinates
     const Eigen::Vector2f robot_location = this->robot.getPose().block<2,1>(0,0);
     
-    // get robot heading
+    // Get robot heading
     const float robot_heading = this->robot.getPose()(2);
     
-    // get robot location in discrete area coordinates
+    // Get robot location in discrete area coordinates
     const Eigen::Vector2i area_robot_location = this->discretize_world_location(robot_location);
     
-    // get robot radius in image coordinates
+    // Get robot radius in image coordinates
     int area_radius = this->scale_world_variable(this->robot.getRadius());
     
-    // draw robot as a circle
+    // Draw robot as a circle
     cv::circle(this->data, {area_robot_location(0), area_robot_location(1)}, area_radius, this->robot_color, -1);
     
-    // indicate orientation of the robot by drawing a line from the origin
+    // Indicate orientation of the robot by drawing a line from the origin
     float line_scale = 1.0; // * robot radius
     int line_endpoint_x = (int)(area_robot_location(0) + line_scale * area_radius * cos(robot_heading));
     int line_endpoint_y = (int)(area_robot_location(1) + line_scale * area_radius * sin(robot_heading));
     cv::line(this->data, {area_robot_location(0), area_robot_location(1)}, {line_endpoint_x, line_endpoint_y}, {255, 255, 255}, 2);
     
-    // draw black circle around robot
+    // Draw black border around robot
     cv::circle(this->data, {area_robot_location(0), area_robot_location(1)}, area_radius, {0, 0, 0}, 2);
 
-    
 }
 
 
-// draw Particles
+// Draw Particles
 void Area::drawParticles(){
     
+    // Iterate over all particles
     for (list<Particle>::iterator it = this->robot.getFilter().getParticles().begin(); it != this->robot.getFilter().getParticles().end(); it++) {
         
-        // get particle location in continuous world coordinates
+        // Get particle location in continuous world coordinates
         const Eigen::Vector2f particle_location = (*it).getPose().block<2,1>(0,0);
         
-        // get particle location in discrete area coordinates
+        // Get particle location in discrete area coordinates
         const Eigen::Vector2i area_particle_location = this->discretize_world_location(particle_location);
         
-        // draw robot as a black circle
+        // Draw particle as dot
         cv::circle(this->data, {area_particle_location(0), area_particle_location(1)}, 6, this->particle_color, -1);
         
     }
-    
 }
 
 
-// *********************
-// Coordinate Transforms
-// *********************
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++ Coordinate Transforms +++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// coordinate transformation
+// Transform continuous world coordinates into discrete area coordinates (m -> px)
 Eigen::Vector2i Area::discretize_world_location(const Eigen::Vector2f& world_location){
     
-    Eigen::Vector2i area_location; // pose in discrete area coordinates
+    // Container for pose in discrete area coordinates
+    Eigen::Vector2i area_location;
     
+    // Discretize x and y coordinates
     area_location(0) = (int)((world_location(0) - x_min) / (x_max - x_min) * data_dim_x);
     area_location(1) = (int)((world_location(1) - y_min) / (y_max - y_min) * data_dim_y);
     
@@ -310,21 +324,15 @@ Eigen::Vector2i Area::discretize_world_location(const Eigen::Vector2f& world_loc
 }
 
 
+// Scale scalar variables from world coordinate system to area coordinate system (m -> px)
 int Area::scale_world_variable(const float& world_variable){
     
+    // Container for scaled area variable
     int area_variable;
+    
+    // Scale variable
     area_variable = (int)(world_variable / this->resolution);
+    
     return area_variable;
     
-}
-
-void Area::summary(){
-    
-    cout << "Area: " << endl;
-    cout << "-----" << endl;
-    cout << "Width: " << this->x_max - this->x_min << "m" << endl;
-    cout << "Height: " << this->y_max - this->y_min << "m" << endl;
-    Map::summary();
-    this->getRobot().summary();
-
 }
--- Area.h
+++ Area.h
@@ -17,14 +17,13 @@
 class Area{
     
     public:
-    // constructor and destructor
+    // Constructor and destructor
     Area();
     Area(vector<vector<float>> wall_coordinates, float x_min, float x_max, float y_min, float y_max, float resolution);
     ~Area(){};
     
-    // drawing functions
-    void drawInitialScene();
-    cv::Mat drawScene(int verbose);
+    // Drawing functions
+    void drawScene(int verbose);
     void drawPath();
     void drawRobot();
     void drawWalls();
@@ -32,40 +31,43 @@ class Area{
     void drawScanEstimate();
     void drawParticles();
     
-    // transforms from continuous world coordinate system to discrete area coordinate system
+    // Transforms from continuous world coordinate system to discrete area coordinate system
     Eigen::Vector2i discretize_world_location(const Eigen::Vector2f& world_location);
     int scale_world_variable(const float& world_coordinate);
     
-    // getter functions
+    // Getter functions
     Robot& getRobot(){ return this->robot; };
+    cv::Mat& getData(){ return this->scene_data; };
     
-    // setter functions
-    void setRobot(Robot robot){ this->robot = robot; };
+    // Setter functions
+    void setRobot(Robot& robot){ this->robot = robot; };
     
-    // print summary
+    // Print summary
     void summary();
 
     private:
-    float x_min;
-    float x_max;
-    float y_min;
-    float y_max;
-    float resolution;
-    int data_dim_x;
-    int data_dim_y;
-    
-    cv::Mat data;
-    Robot robot;
-    
-    vector<vector<float>> wall_coordinates;
-    
-    // colors
-    cv::Scalar robot_color = {130, 130, 130};
-    cv::Scalar particle_color = {60, 60, 60};
-    cv::Scalar scan_color = {80, 127, 255};
-    cv::Scalar scan_estimate_color = {255, 127, 80};
-    cv::Scalar path_ground_truth_color = {80, 127, 255};
-    cv::Scalar path_estimate_color = {255, 127, 80};
+        float x_min; // minimum x-coordinate of area
+        float x_max; // maximum x-coordinate of area
+        float y_min; // minimum y-coordinate of area
+        float y_max; // maximum y-coordinate of area
+        float resolution; // resolution of scene image in m/px
+        int data_dim_x; // width of the scene image in px
+        int data_dim_y; // height of the scene image in px
+        
+        cv::Mat data; // image on which scene is drawn
+        cv::Mat scene_data; // image which contains currently active scene (for result saving)
+        Robot robot; // robot object
+        
+        // Vector containing all wall coordiantes in area
+        vector<vector<float>> wall_coordinates;
+        
+        // Colors
+        cv::Scalar robot_color = {130, 130, 130};
+        cv::Scalar particle_color = {60, 60, 60};
+        cv::Scalar scan_color = {80, 127, 255};
+        cv::Scalar scan_estimate_color = {255, 127, 80};
+        cv::Scalar path_ground_truth_color = {80, 127, 255};
+        cv::Scalar path_estimate_color = {255, 127, 80};
 
 };
 
